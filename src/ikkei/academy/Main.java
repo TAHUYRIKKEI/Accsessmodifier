7cb53b6c8ae7e4ba36e395f550a43eb8525841fb
unknown <tavanhuyvp8590@gmail.com>
2022-08-01 00:47:49 +0700

    BT5

---
+++ src/ikkei/academy/Main.java
@@ -0,0 +1,11 @@
+package ikkei.academy;
+
+import Circle.Circle;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle= new Circle(5,"blue");
+        System.out.println("ban kinh la: "+circle.getRadius());
+        System.out.println("dien tich la: "+circle.getArea());
+    }
+}
